2c803155acc23603730a52a7a4460a42de8dcf8d
Siddharth Garimella <gsidsid@gmail.com>
2018-12-09 14:23:49 -0500

    heroku port

--- Web/front.py
+++ Web/front.py
@@ -33,5 +33,5 @@ def submit():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 33525))
+    port = int(os.environ.get('PORT', 33507))
     application.run(host='0.0.0.0',port=port)
