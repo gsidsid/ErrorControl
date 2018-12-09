7bfd5958c490eb4cf1218af6d9fea5919a19e42e
Siddharth Garimella <gsidsid@gmail.com>
2018-12-09 14:49:01 -0500

    what is wrong with heroku help

--- Web/front.py
+++ Web/front.py
@@ -34,5 +34,5 @@ def submit():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 33599))
+    port = int(os.environ.get('PORT', 33507))
     application.run(host='0.0.0.0',port=port)
