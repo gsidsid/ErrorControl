f857a12d30fa015ea643394ef6b272a1c19008a4
Siddharth Garimella <gsidsid@gmail.com>
2018-12-09 12:50:07 -0500

    web works?

--- Web/static/script.js
+++ Web/static/script.js
@@ -1,1 +1,12 @@
-console.log(document.getElementById('message').value)
+
+var elem = document.getElementById('message');
+elem.addEventListener('keypress', function(e){
+  if (e.keyCode == 13) {
+    console.log(elem.value);
+  }
+});
+
+console.log(result);
+elem.value = result;
+
+document.getElementById('paritycheck').value = res_a;
